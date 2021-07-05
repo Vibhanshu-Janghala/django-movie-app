0dba028feaef00a4c9a183f395dd510989383775
Vibhanshu-Janghala <vibhanshujanghala1@gmail.com>
2021-07-06 00:43:58 +0530

    Added Image functionality

--- movie_handler/admin.py
+++ movie_handler/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Movie
-# Register your models here.
 
-admin.site.register(Movie)
+
+# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre')
+
+
+admin.site.register(Movie, MovieAdmin)
--- movie_handler/views.py
+++ movie_handler/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    return render(request, "index.html", {"movies": movies})
+
+
+def detail(request, movie_title):
+    movie = Movie.objects.get(title=movie_title)
+    return render(request, "moviedetail.html", {"movie": movie})
